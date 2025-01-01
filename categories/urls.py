b1a4c4777943bf118a71e384c19cf4ef9708de85
Valerie Solinas <valerius92@live.it>
2025-01-01 14:21:56 +0000

    update serializer,urls,views for profile, update urls for categ and main

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from categories import views
 
 urlpatterns = [
-    path('categories/', views.CategoryList.as_view()),
+    path('', views.CategoryList.as_view()),
 ]
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
-
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Profile
         fields = [
