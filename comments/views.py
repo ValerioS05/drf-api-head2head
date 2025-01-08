1be53638ba7ae9b6cd52f5ded0877a4d24f2f4cb
Valerie Solinas <valerius92@live.it>
2025-01-08 09:22:27 +0000

    refactor comments views as generics and add filters

--- comments/views.py
+++ comments/views.py
@@ -1,110 +1,37 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
-from .models import Product
 from .serializers import CommentSerializer
 from drf_api_head2head.permissions import IsOwnerOrReadOnly
 
-class CommentList(APIView):
+class CommentList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
     ]
-    
-    def get(self, request):
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(
-            comments,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return Response(
-                'You must be logged in to leave a comment.',
-                status=status.HTTP_403_FORBIDDEN
-            )
-        
-        product_id = request.data.get('product')
-        if not product_id:
-            return Response(
-                {'Product not specified.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-    
-    
-        try:
-            product = Product.objects.get(id=product_id)
-        except Product.DoesNotExist:
-            return Response(
-                {'Invalid product.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        
-        serializer = CommentSerializer(
-            data=request.data,
-            context={
-                'request' : request
-            }
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user, product=product)
-            return Response(
-                serializer.data,
-                status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+    search_fields = [
+        'owner__username',
+        'content',
+        'product__name',
+    ]
+    ordering_fields = [
+        'owner__username',
+        'created_at',
+    ]
+    filterset_fields = [
+        'owner__username',
+        'product',
+    ]
 
-class CommentDetail(APIView):
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    
-    def get_comment(self, pk):
-        try:
-            comment = Comment.objects.get(pk=pk)
-            self.check_object_permissions(self.request, comment)
-            return comment
-        except Comment.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        comment = self.get_comment(pk)
-        self.check_object_permissions(self.request, comment)
-        serializer = CommentSerializer(
-            comment,
-            context= {
-                'request' : request
-            }
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        comment = self.get_comment(pk)
-        self.check_object_permissions(self.request, comment)
-        serializer = CommentSerializer(
-            comment,
-            data=request.data,
-            context = {
-                'request' : request
-            }
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-        
-    def delete(self, request, pk):
-        comment = self.get_comment(pk)
-        self.check_object_permissions(self.request, comment)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
