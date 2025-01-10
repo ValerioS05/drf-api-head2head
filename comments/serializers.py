b54dc1cba0a1a9d26f3fc9462d900f6b5e4fd124
Valerio Solinas <valerius92@live.it>
2025-01-10 16:57:42 +0000

    Add root route and time format/pagination in settings, modify comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,11 +1,16 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 from .models import Product
 
 class CommentSerializer(serializers.ModelSerializer):
     product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
-    created_at = serializers.ReadOnlyField()
+    created_at = serializers.SerializerMethodField()
     owner = serializers.ReadOnlyField(source='owner.username')
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
     class Meta:
         model = Comment
         fields = [
