79029af9e2cbb513568039770bbef2ce7ad2c646
Valerie Solinas <valerius92@live.it>
2025-01-11 18:31:05 +0000

    Fixed comparison serializers.py

--- comparisons/serializers.py
+++ comparisons/serializers.py
@@ -6,12 +6,13 @@ class ComparisonSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     created_at = serializers.ReadOnlyField()
     products = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), many=True)
+    
     def validate_products(self, value):
         if len(value) != 2:
             raise serializers.ValidationError(
                 'You must select exactly two products to compare.'
                 )
-        return value
+
         if value[0] == value[1]:
             raise serializers.ValidationError(
                 "Can't compare the same product."
