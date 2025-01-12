2a7c75a4f839a985df88c89ba915e3e80c4a6082
Valerie Solinas <valerius92@live.it>
2025-01-12 17:38:37 +0000

    Fixed validation in categories models.py

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.core.exceptions import ValidationError
 
-class Category(models.Model) :
+class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     image = CloudinaryField(
@@ -15,20 +16,18 @@ class Category(models.Model) :
         """
         Simple validation for image size, format, and dimensions.
         """
+        max_size = 5 * 1024 * 1024  # 5MB
 
-        max_size = 5 * 1024 * 1024
-        if self.image.size > max_size:
-            raise ValidationError('Image size must be less than 5MB.')
+        if self.image:
+            if self.image.size > max_size:
+                raise ValidationError('Image size must be less than 5MB.')
 
-        formats = ('.png', '.jpg', '.jpeg')
-        if not self.image.name.endswith(formats):
-            raise ValidationError('Image must be in .png, .jpg, .jpeg format.')
+            formats = ('.png', '.jpg', '.jpeg')
+            if not self.image.name.lower().endswith(formats):
+                raise ValidationError('Image must be in .png, .jpg, .jpeg format.')
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
-
-    def __str__(self):
-        return self.name
