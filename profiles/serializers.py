b40a14e7f355cb070e865f40e6007026ea3279cb
Valerie Solinas <valerius92@live.it>
2025-01-18 13:33:52 +0000

    Add function to retrieve cloudinary url in profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,16 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_picture = serializers.SerializerMethodField()
 
-    def get_is_owner(self,obj):
+    def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_profile_picture(self, obj):
+        if obj.profile_picture:
+            return obj.profile_picture.url
+        return None 
 
     class Meta:
         model = Profile
